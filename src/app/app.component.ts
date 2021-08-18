72d81f389f27deaba769d319b9f669daf10c460f
billcuthbert <96billc@gmail.com>
2021-08-18 01:37:07 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,50 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  grid: any[][] = Array.from(Array(80), () => new Array(80).fill('white'));
-  grid2: any[][] = Array.from(Array(80), () => new Array(80).fill('white'));
+
+  empty = () => Array.from(Array(80), () => new Array(80).fill('white'))
+
+  grid: any[][] = this.empty()
+  grid2: any[][] = this.empty()
+
+  mouseDown = false
 
   ngOnInit() {
-    setInterval(() => this.update(), 1000);
+    setInterval(() => this.update(), 5000)
   }
 
-  click(i: number, j: number) {
-    console.log(i, j);
-    this.grid2[i][j] = (this.grid[i][j] == 'white' ? 'black' : 'white');
-    this.grid[i][j] = (this.grid[i][j] == 'white' ? 'black' : 'white');
+  click(x: number, y: number) {
+    this.mouseDown = true
+    this.change(x, y)
+  }
 
+  change(i: number, j: number) {
+    const next = this.grid[i][j] == 'white' ? 'black' : 'white'
+    this.grid2[i][j] = next
+    this.grid[i][j] = next
+  }
+
+  over(x: number, y: number) {
+    if (this.mouseDown)
+      this.change(x, y)
+  }
+
+  stop() {
+    this.mouseDown = false
+  }
+
+  liveNeighbours(x: number, y: number, l: boolean) {
+    let n = l ? -1 : 0
+    for (let i = x - 1; i < x + 2; i++) {
+      for (let j = y - 1; j < y + 2; j++) {
+        if (this.grid[i] && this.grid[i][j] && this.grid[i][j] == 'black')
+          n++
+      }
+    }
+    return n
   }
 
   update() {
-    const liveNeighbours = (x: number, y: number, l: boolean) => {
-      let n = (l ? -1 : 0)
-      for (let i = x - 1; i < x + 2; i++) {
-        for (let j = y - 1; j < y + 2; j++) {
-          if (this.grid[i] && this.grid[i][j] && this.grid[i][j] == 'black')
-            n++
-        }
-      }
-      return n
-    }
-
     for (let i = 0; i < this.grid.length; i++) {
       for (let j = 0; j < this.grid[0].length; j++) {
         const alive = this.grid[i][j] == 'black'
-        const ns = liveNeighbours(i, j, alive)
-
-        let r = false
-        if (alive) {
-          if (ns == 2 || ns == 3) r = true
-        } else {
-          if (ns == 3) r = true
-        }
-        // if ((alive && ns == 2) || ns == 3) r = true
-
-        this.grid2[i][j] = r ? 'black' : 'white'
+        const ns = this.liveNeighbours(i, j, alive)
+        const live = ((alive && ns == 2) || ns == 3)
+        this.grid2[i][j] = live ? 'black' : 'white'
       }
     }
 
     this.grid = this.grid2
-    this.grid2 = Array.from(Array(80), () => new Array(80).fill('white'));
+    this.grid2 = this.empty()
   }
 }
